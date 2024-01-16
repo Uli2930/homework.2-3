73e5774782653eb8a31b2563b3f742ad8b4ddd7b
Amin_stors <uli2930tong@gmail.com>
2024-01-16 21:15:25 +0300

    hww3

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,27 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        BankAccount bankAccount = new BankAccount();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        bankAccount.deposit(15000);
+
+        System.out.println("У вас на счёте: " + bankAccount.getAmount() + " сом.");
+
+        while (true) {
+            try {
+                bankAccount.withDraw(6000);
+                System.out.println("Указанная вами сумма успешно снята со счёта! \nУ вас на счёте: " + bankAccount.getAmount() + " сом.");
+            } catch (LimitException e) {
+                System.out.println(e.getMessage());
+                try {
+                    bankAccount.withDraw((int) bankAccount.getAmount());
+                } catch (LimitException ex) {
+                    throw new RuntimeException(ex);
+                }
+                System.out.println("Оставшаяся сумма успешно снята со счёта!");
+                System.out.println("Остаток на счёте: " + bankAccount.getAmount() + " сом.");
+                break;
+            }
         }
     }
 }
